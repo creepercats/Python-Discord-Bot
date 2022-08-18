2c277731cad9309cc534a879fa8911414ba0dbaa
Creepercats <Creepercatsgaming@gmail.com>
2022-08-18 02:39:16 -0400

    Basic Discord Bot Setup

--- main.py
+++ main.py
@@ -1,1 +1,20 @@
-print("Hello, World!");
+import discord
+from dotenv import dotenv_values
+
+config = dotenv_values(".env")
+
+
+class MyClient(discord.Client):
+    async def on_ready(self):
+        print(f'Logged on as {self.user}!')
+
+    async def on_message(self, message):
+        print(f'Message from {message.author}: {message.content}')
+
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+client = MyClient(intents=intents)
+client.run(config.get("DISCORD_KEY"))
+
